e25977bd17b71e9fddf7c4037fdea7f77ad528fa
Maxime Leboeuf <ml@maximeleboeuf.com>
2015-08-03 14:03:19 -0400

    Make Backtest's BacktestingContext public. Console output.

--- TradeHub.Backtesting.ConsoleClient/Program.cs
+++ TradeHub.Backtesting.ConsoleClient/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using TradeHub.Backtesting.Framework;
 using TradeHub.Core.DataProviders;
+using TradeHub.Core.Model;
 using TradeHub.Core.Model.Enums;
 
 namespace TradeHub.Backtesting.ConsoleClient
@@ -16,12 +18,84 @@ namespace TradeHub.Backtesting.ConsoleClient
 
         static async Task MainAsync(string[] args)
         {
-            var backtest = new Backtest();
-            backtest.SetStockData(await YahooHistoricalDataProvider.DownloadHistoricalData("YHOO", new DateTime(2015, 01, 02), DateTime.Now));
+            WriteTitle();
+            WriteInfo("Setting up backtest...");
 
-            backtest.PlaceTradeOrder(TradeOrderAction.Buy, "YHOO", new DateTime(2015, 01, 02), 38.18m, 100);
+            var backtest = new Backtest();
+            var portfolio = new Portfolio
+            {
+                CashBalance = 100000
+            };
+
+            backtest.Context.Portfolio = portfolio;
+
+            WriteInfo("Downloading historical data...");
+            backtest.Context.StockData = await YahooHistoricalDataProvider.DownloadHistoricalData("YHOO", new DateTime(2015, 01, 02), DateTime.Now);
+
+            WriteInfo("Placing input trade orders...");
+            backtest.PlaceTradeOrder(TradeOrderAction.Buy, "YHOO", new DateTime(2015, 01, 02), 38.18m, 1000);
+
+            WriteInfo("Backtest setup done.");
+            WriteBacktestSetup(backtest);
+
+            var stopwatch = new Stopwatch();
+            WriteInfo("Backtest starts.");
+            stopwatch.Start();
 
             backtest.Run();
+
+            stopwatch.Stop();
+            WriteInfo("Backtest finished.");
+            Console.WriteLine("Elapsed: {0} seconds ({1})", stopwatch.Elapsed.TotalSeconds, stopwatch.Elapsed);
+
+            WriteBacktestResults(backtest);
+
+            Console.ReadKey();
+        }
+
+        private static void WriteTitle()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("*************************** ");
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.Write("TradeHub Backtesting CLI");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" ***************************");
+            Console.ResetColor();
+        }
+
+        private static void WriteBacktestSetup(Backtest backtest)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Backtest setup");
+            Console.WriteLine(" Portfolio cash balance: {0}", backtest.Context.Portfolio.CashBalance);
+            Console.WriteLine(" Portfolio positions: {0}", backtest.Context.Portfolio.Positions.Count);
+            Console.WriteLine(" Days of historical data: {0}", backtest.Context.StockData.Count);
+            Console.WriteLine(" Reference timeframe: {0} to {1}", backtest.Context.StockData[0].Timestamp.ToShortDateString(), backtest.Context.StockData[backtest.Context.StockData.Count - 1].Timestamp.ToShortDateString());
+            Console.WriteLine();
+        }
+
+        private static void WriteBacktestResults(Backtest backtest)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Backtest results");
+            Console.WriteLine(" Portfolio cash balance: {0}", backtest.Context.Portfolio.CashBalance);
+            Console.WriteLine(" Portfolio positions: {0}", backtest.Context.Portfolio.Positions.Count);
+
+            var totalPortfolioValue = backtest.Context.Portfolio.CashBalance;
+            foreach (var position in backtest.Context.Portfolio.Positions)
+            {
+                totalPortfolioValue += position.Quantity * backtest.Context.StockData[backtest.Context.StockData.Count - 1].Close;
+            }
+            Console.WriteLine(" Portfolio total value: {0}", totalPortfolioValue);
+
+            Console.WriteLine();
+        }
+
+        private static void WriteInfo(string message)
+        {
+            Console.WriteLine("{0:yyyy-MM-dd HH:mm:ss.fff}: {1}", DateTime.Now, message);
         }
     }   
 }
--- TradeHub.Backtesting.Framework/Backtest.cs
+++ TradeHub.Backtesting.Framework/Backtest.cs
@@ -11,27 +11,14 @@ namespace TradeHub.Backtesting.Framework
         /// <summary>
         /// The context data for this backtest.
         /// </summary>
-        private BacktestingContext context = new BacktestingContext();
-
-        /// <summary>
-        /// Set the stock data that will be used for the backtest.
-        /// </summary>
-        /// <param name="StockData"></param>
-        public void SetStockData(List<StockTick> StockData)
-        {
-            context.StockData = StockData;
-            context.Portfolio = new Portfolio
-            {
-                CashBalance = 100000
-            };
-        }
+        public BacktestingContext Context = new BacktestingContext();
 
         /// <summary>
         /// Save a trade order to be executed at a certain date in the simulation.
         /// </summary>
         public void PlaceTradeOrder(TradeOrderAction action, string symbol, DateTime timestamp, decimal price, int quantity)
         {
-            context.InputTransactions.Add(new TradeOrder
+            Context.InputTransactions.Add(new TradeOrder
             {
                 Action = action,
                 Symbol = symbol,
@@ -49,13 +36,13 @@ namespace TradeHub.Backtesting.Framework
         /// </remarks>
         public void Run()
         {
-            for (context.CurrentIteration = 0; context.CurrentIteration < context.StockData.Count(); ++context.CurrentIteration)
+            for (Context.CurrentIteration = 0; Context.CurrentIteration < Context.StockData.Count(); ++Context.CurrentIteration)
             {
-                var currentStockTick = context.StockData[context.CurrentIteration];
+                var currentStockTick = Context.StockData[Context.CurrentIteration];
 
                 // Check if there is any transaction pending
                 var currentDate = currentStockTick.Timestamp.Date;
-                var transactionsForCurrentDate = context.InputTransactions.Where(t => t.Timestamp.Date == currentDate).ToList();
+                var transactionsForCurrentDate = Context.InputTransactions.Where(t => t.Timestamp.Date == currentDate).ToList();
                 if (transactionsForCurrentDate.Any())
                 {
                     ExecuteTradeOrders(transactionsForCurrentDate);
@@ -75,7 +62,7 @@ namespace TradeHub.Backtesting.Framework
 
         private void ExecuteTradeOrder(TradeOrder tradeOrder)
         {
-            BacktestingPortfolioManager.ExecuteTradeOrder(tradeOrder, context.Portfolio);
+            BacktestingPortfolioManager.ExecuteTradeOrder(tradeOrder, Context.Portfolio);
         }
     }
 }
