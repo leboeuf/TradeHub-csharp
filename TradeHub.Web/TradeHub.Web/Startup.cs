6fba4d3681e9d635416f4513f1b3b92eb3b45e6f
Maxime Leboeuf <ml@maximeleboeuf.com>
2016-02-13 22:40:52 -0500

    Create TradeHub.Web project

---
+++ TradeHub.Web/TradeHub.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TradeHub.Web.Startup))]
+namespace TradeHub.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
