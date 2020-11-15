008e133cf679b0de0b9f84667445580a036e652d
mleboeuf <ml@maximeleboeuf.com>
2020-11-15 13:10:17 -0500

    Update TSX Indices

--- TradeHub.Core.Model/Enums/Indices/Tsx.cs
+++ TradeHub.Core.Model/Enums/Indices/Tsx.cs
@@ -16,17 +16,11 @@
             "^TX60U", // S&P/TSX 60 Index (USD)
             "^TX60UH", // S&P/TSX 60 Hedged Index (USD
             "^TX6UHN", // S&P/TSX 60 Hedged Index (USD Net TR)
-            "^TESG", // S&P/TSX 60 ESG Index
-            "^TESGN", // S&P/TSX 60 ESG Index (Net TR)
             "^TXSI", // S&P/TSX 60 Shariah Index
             "^TXCA", // The S&P/TSX 60 Carbon Efficient Index
             "^TXCAU", // The S&P/TSX 60 Carbon Efficient Index (USD)
             "^TXCAN", // The S&P/TSX 60 Carbon Efficient Index (Net TR)
             "^TXCANU", // The S&P/TSX 60 Carbon Efficient Index (USD Net TR)
-            "^TXCSC", // S&P/TSX 60 Carbon Efficient Select Index
-            "^TXCSN", // S&P/TSX 60 Carbon Efficient Select Index (Net TR)
-            "^TXCSCU", // S&P/TSX 60 Carbon Efficient Select Index (USD)
-            "^TXCSUN", // S&P/TSX 60 Carbon Efficient Select Index (USD Net TR)
             "^TXFF", // The S&P/TSX 60 Fossil Fuel Free Index
             "^TXFFN", // The S&P/TSX 60 Fossil Fuel Free Index (Net TR)
             "^TXFFU", // The S&P/TSX 60 Fossil Fuel Free Index (USD)
@@ -35,12 +29,10 @@
             "^TXFCN", // S&P/TSX 60 Fossil Fuel Free Carbon Efficient Index (Net TR)
             "^TXFCU", // S&P/TSX 60 Fossil Fuel Free Carbon Efficient Index (USD)
             "^TXFCNU", // S&P/TSX 60 Fossil Fuel Free Carbon Efficient Index (USD Net TR)
-            "^TXFCS", // S&P/TSX 60 Fossil Fuel Free Carbon Efficient Select Index
-            "^TXFCSN", // S&P/TSX 60 Fossil Fuel Free Carbon Efficient Select Index (NTR)
-            "^TXFCSU", // S&P/TSX 60 Fossil Fuel Free Carbon Efficient Select Index (USD)
-            "^TFCSNU", // S&P/TSX 60 Fossil Fuel Free Carbon Efficient Select Index (USD Net TR)
+            "^TIMM", // S&P/TSX Composite Metals and Mining Industry Index
             "^TX40", // S&P/TSX Completion Index
             "^TX20", // S&P/TSX SmallCap Index
+            "^TXSC", // S&P/TSX SmallCap Select Index (CAD)
             "^RTCM", // S&P/TSX Income Trust Index
             "^TXEI", // S&P/TSX Composite High Dividend Index (CAD)
             "^TXEU", // S&P/TSX Composite High Dividend Index (USD)
@@ -49,19 +41,9 @@
             "^TXBBU", // S&P/TSX Composite Buyback Index (USD)
             "^TXBBN", // S&P/TSX Composite Buyback Index (CAD) (Net Total Return)
             "^TXBBNU", // S&P/TSX Composite Buyback Index (USD) (Net Total Return)
-            "^TXSY", // S&P/TSX Composite Shareholder Yield Index (CAD)
-            "^TXSYU", // S&P/TSX Composite Shareholder Yield Index (USD)
-            "^TXSYN", // S&P/TSX Composite Shareholder Yield Index (CAD) (Net Total Return)
-            "^TXSYNU", // S&P/TSX Composite Shareholder Yield Index (USD) (Net Total Return)
             "^TXHE", // S&P/TSX High Income Energy Index (CAD)
             "^TXHU", // S&P/TSX High Income Energy Index (USD)
             "^TXPR", // S&P/TSX Preferred Share Index
-            "^TXPL", // S&P/TSX Preferred Share Laddered Index
-            "^TXLC", // S&P/TSX Preferred Share Current Year Laddered Index
-            "^TXL1", // S&P/TSX Preferred Share Year 1 Laddered Index
-            "^TXL2", // S&P/TSX Preferred Share Year 2 Laddered Index
-            "^TXL3", // S&P/TSX Preferred Share Year 3 Laddered Index
-            "^TXL4", // S&P/TSX Preferred Share Year 4 Laddered Index
             "^STNC", // S&P/TSX North American Preferred Stock Index (Price Return) (CAD)
             "^STNU", // S&P/TSX North American Preferred Stock Index (Price Return) (USD)
             "^STNCH", // S&P/TSX North American Preferred Stock Index (Price Return) (CAD Hedged)
@@ -70,12 +52,17 @@
             "^STNCHN", // S&P/TSX North American Preferred Stock Index (Net Total Return) (CAD Hedged)
             "^TXDV", // S&P/TSX Canadian Dividend Aristocrats Index
             "^TXDC", // S&P/TSX Composite Dividend Index
+            "^TXDP", // S&P/TSX 60 Dividend Points Index
+            "^TXDPA", // S&P/TSX 60 Dividend Points Index (Annual)
             "^TXCT", // S&P/TSX Renewable Energy and Clean Technology Index
+            "^XCAN", // S&P/TSX Cannabis Index (CAD)
+            "^XCANN", // S&P/TSX Cannabis Index (USD) NTR
             "^TTGD", // S&P/TSX Global Gold Index
             "^TXGU", // S&P/TSX Global Gold Index (USD)
             "^TXGM", // S&P/TSX Global Mining Index
             "^TXBM", // S&P/TSX Global Base Metals Index
             "^TXBMU", // S&P/TSX Global Base Metals Index (USD)
+            "^MCAN", // S&P/TSX International Cannabis Index (CAD)
             "^SPTSEN", // S&P/TSX 60 Index NTR
             "^TXHB", // S&P/TSX Composite High Beta Index
             "^TXLV", // S&P/TSX Composite Low Volatility Index
@@ -87,7 +74,6 @@
             "^TX6C", // S&P/TSX 60 Capped Index
             "^TTCD", // S&P/TSX Capped Consumer Discretionary Index
             "^TTCS", // S&P/TSX Capped Consumer Staples Index
-            "^TTMN", // S&P/TSX Capped Diversified Metals & Mining Index
             "^TTEN", // S&P/TSX Capped Energy Index
             "^TTFS", // S&P/TSX Capped Financial Index
             "^TTHC", // S&P/TSX Capped Health Care Index
@@ -98,6 +84,7 @@
             "^TTTS", // S&P/TSX Capped Telecommunication Services Index
             "^TTUT", // S&P/TSX Capped Utilities Index
             "^RTRE", // S&P/TSX Capped REIT Index
+            "^SPTRED", // S&P/TSX Capped REIT Income Index
         };
 
         public static string[] EqualWeight = {
@@ -111,13 +98,36 @@
             "^TXBEH", // S&P/TSX Equal Weight Global Base Metals CAD Hedged Index
         };
 
-        public static string[] Volatility = {
-            "^VIXC", // S&P/TSX 60 VIX
+        public static string[] Factor = {
+            "^TXEV", // S&P/TSX Composite Enhanced Value Index
+            "^TXEVLQ", // S&P/TSX Composite Enhanced Value - Lowest Quintile Index
+            "^TXQ", // S&P/TSX Composite Quality Index
+            "^TXQLQ", // S&P/TSX Composite Quality - Lowest Quintile Index
+            "^TVXHQ", // S&P/TSX Composite Quality - Highest Quintile Index
+            "^TXM", // S&P/TSX Composite Momentum Index
+            "^TXMLQ", // S&P/TSX Composite Momentum - Lowest Quintile Index
+            "^TXLVHD", // S&P/TSX Composite Low Volatility High Dividend Index
         };
 
         public static string[] Venture = {
             "^JX", // S&P/TSX Venture Composite Index
-            "^VXSC", // S&P/TSX Venture Select Index
+            "^JX10", // S&P/TSX Venture Energy (Sector) Index (CAD)
+            "^JX15", // S&P/TSX Venture Materials (Sector) Index (CAD)
+            "^JX20", // S&P/TSX Venture Industrials (Sector) Index (CAD)
+            "^JX25", // S&P/TSX Venture Consumer Discretionary (Sector) Index (CAD)
+            "^JX30", // S&P/TSX Venture Consumer Staples (Sector) Index (CAD)
+            "^JX35", // S&P/TSX Venture Health Care (Sector) Index (CAD)
+            "^JX40", // S&P/TSX Venture Financials (Sector) Index (CAD)
+            "^JX45", // S&P/TSX Venture Information Technology (Sector) Index (CAD)
+            "^JX60", // S&P/TSX Venture Real Estate (Sector) Index (CAD
+            "^JX3520", // S&P/TSX Venture Pharmaceuticals, Biotechnology & Life Sciences (Industry Group) Index (CAD)
+            "^JX4510 ", // S&P/TSX Venture Software & Services (Industry Group) Index (CAD)
+            "^JX101020", // S&P/TSX Venture Oil, Gas & Consumable Fuels (Industry) Index (CAD)
+            "^JX151040", // S&P/TSX Venture Metals & Mining (Industry) Index (CAD)
+            "^JX10102020", // S&P/TSX Venture Oil & Gas Exploration & Production (Sub Industry) Index (CAD)
+            "^JX15104020", // S&P/TSX Venture Diversified Metals & Mining (Sub Industry) Index (CAD)
+            "^JX15104030 ", // S&P/TSX Venture Gold (Sub Industry) Index (CAD)
+            "^JX15104040", // S&P/TSX Venture Precious Metals & Minerals (Sub Industry) Index (CAD)
         };
     }
 }
